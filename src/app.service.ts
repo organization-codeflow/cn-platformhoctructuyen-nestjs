36271e5807e60d7ac19c93fd5b783968ee4ee92d
tanmai <tanmai833@gmail.com>
2025-08-29 02:16:04 +0700

    fix: update greeting message in AppService to specify API version

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Welcome to Learning Platform';
+    return 'Hello World! Welcome to Learning Platform API';
   }
 }
